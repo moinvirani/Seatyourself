6fac4f3b5306ebe53cffb5bc8ecfb0847b2d95dd
Moin Virani <moin.virani@gmail.com>
2013-11-19 18:22:03 -0500

    Added more fields to the reservation form

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,7 +4,7 @@ class Reservation < ActiveRecord::Base
 	validates :people, :numericality => { :only_integer => true, :greater_than => 0, :less_than => 500}
 	
 
- 	belongs_to :restaurant
+ 	  belongs_to :restaurant
   	belongs_to :user
 
   	
